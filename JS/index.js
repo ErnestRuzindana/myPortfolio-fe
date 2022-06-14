fa0f876b53e5fa4bed1b5e183aed8066056cb41f
Ernest Ruzindana <ruzindana.ernest99@gmail.com>
2022-06-14 09:19:18 +0200

    blogsection

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,20 @@
+
 const toggleButton = document.getElementById ("toggleButton");
 
 const navlinks = document.getElementById ("navlinks");
 
 const closeButton = document.getElementById("closeButton");
 
-
 navlinks.style.display = "none";
 
-toggleButton.addEventListener ("click", function(){
-    navlinks.style.display = "block";
-})
+if (window.innerWidth = 600) {
+    toggleButton.addEventListener ("click", function(){
+        navlinks.style.display = "block";
+    })
 
-closeButton.addEventListener("click", function(){
-    navlinks.style.display = "none";
-})
+    closeButton.addEventListener("click", function(){
+        navlinks.style.display = "none";
+    })
+}
+
 
