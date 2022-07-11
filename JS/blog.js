ce8cfc62137613b5540b447b4e9151ce7d6b2eb8
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2022-07-11 13:32:22 -0700

    message

--- JS/blog.js
+++ JS/blog.js
@@ -106,14 +106,3 @@ document.addEventListener('mouseover', function() {
    mousein = true
    reset()
 })
-
-
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-const marqueeContent = document.querySelector("ul.marquee-content");
-
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
-
-for(let i=0; i<marqueeElementsDisplayed; i++) {
-  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
