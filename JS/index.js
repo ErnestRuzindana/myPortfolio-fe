0d3194d428e80d76e129a120c73d23d6ca1fced5
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2022-07-13 19:59:21 -0700

    worked on testimonial section

--- JS/index.js
+++ JS/index.js
@@ -17,37 +17,6 @@ nav.addEventListener("click", function(){
 })
 
 
-//Homeboxes Horizontal Scrollbar
-
-const sliders = document.querySelector('.homeboxes');
-var ImagePadding = 45;
-var scrollPerClick = document.querySelector('.boxes').clientWidth + ImagePadding;
-var ScrollAmount = 0;
-
-function SliderScrollLeft(){
-    sliders.scrollTo({
-        top: 0,
-        left: (ScrollAmount -= scrollPerClick),
-        behavior: 'smooth'
-    });
-
-    if(ScrollAmount < 0) {
-        ScrollAmount = 0;
-    }
-}
-
-function SliderScrollRight(){
-    if(ScrollAmount <= sliders.scrollWidth - sliders.clientWidth){
-    sliders.scrollTo({
-        top: 0,
-        left: (ScrollAmount += scrollPerClick),
-        behavior: 'smooth'
-    });
-
-}
-
-}
-
 
 // Navigation Active links
 
