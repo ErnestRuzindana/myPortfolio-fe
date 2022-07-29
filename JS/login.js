d6aa0e9c477758e819c1daf41c573e36d049455d
Ernest Ruzindana <ruzindana.ernest99@gmail.com>
2022-07-30 00:38:50 +0200

    connecting the signup and login to the frontend

---
+++ JS/login.js
@@ -0,0 +1,60 @@
+const loginForm = document.getElementById("loginForm");
+const user_email = document.getElementById("email");
+const user_password = document.getElementById("password");
+const submit = document.getElementById("submit");
+const loginMessage = document.getElementById("loginMessage");
+
+loginMessage.style.display = "none"
+
+submit.addEventListener("click", (event)=>{
+    event.preventDefault();
+    loginMessage.style.display = "block";
+
+    loginMessage.innerHTML = `<img src="../images/Spinner.gif" alt="Loading..." width="50px" height="50px">`
+
+    login();
+});
+
+function login(){
+    const data = {
+        email: user_email.value,
+        password: user_password.value
+    }
+
+    const sendData = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: new Headers({'Content-Type': 'application/json; charset=UTF-8'})
+    }
+
+    fetch("http://localhost:5000/login/loginUser", sendData)
+    .then(response => response.json())
+    .then((fetchedData)=>{
+        console.log(fetchedData)
+    
+        if (fetchedData.invalidEmail){
+            loginMessage.style.color = "red"
+            loginMessage.innerHTML = fetchedData.invalidEmail
+        }
+
+        else if (fetchedData.invalidPassword){
+            loginMessage.style.color = "red"
+            loginMessage.innerHTML = fetchedData.invalidPassword
+        }
+
+        else if (fetchedData.successMessage){
+            loginMessage.style.color = "green"
+            loginMessage.innerHTML = fetchedData.successMessage
+
+            sessionStorage.setItem("token", JSON.stringify(fetchedData.token))
+            setTimeout(()=>{location = "blog.html"}, 500)
+        }
+
+        else{
+            loginMessage.style.color = "red"
+            loginMessage.innerHTML = fetchedData.errorMessage
+        }
+
+    })
+
+}
