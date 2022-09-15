309b1682f8c2d45209f44c37210a97e8d81867d8
Ernest Ruzindana <ruzindana.ernest99@gmail.com>
2022-09-15 23:13:53 +0200

    finalization about the resetting password

---
+++ JS/forgotPassword.js
@@ -0,0 +1,66 @@
+
+const forgotPasswordForm = document.getElementById("forgotPasswordForm");
+
+const forgotPasswordSubmitData = document.getElementById("forgotPasswordSubmitData");
+
+const forgotPasswordMessage = document.getElementById("forgotPasswordMessage");
+forgotPasswordMessage.style.display = "none";
+
+forgotPasswordSubmitData.addEventListener("click", (event) =>{
+    event.preventDefault();
+    forgotPasswordMessage.style.display = "block";   
+    forgotPasswordMessage.innerHTML = `<img src="../images/Spinner.gif" alt="Loading..." width="50px" height="50px">`
+
+    forgotPassword();
+});
+
+
+function forgotPassword(){
+    
+    const forgotPasswordEmail = document.getElementById("forgotPasswordEmail");
+
+    const data = {
+        email: forgotPasswordEmail.value,
+    }
+
+    const sendData = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: new Headers({'Content-Type': 'application/json; charset=UTF-8'})
+    }
+
+fetch("http://localhost:5000/login/forgotPassword", sendData)
+.then(response => response.json())
+.then((forgotPasswordFetchedData)=>{
+    console.log(forgotPasswordFetchedData)
+
+    if (forgotPasswordFetchedData.invalidEmail){
+        forgotPasswordMessage.style.color = "red"
+        forgotPasswordMessage.innerHTML = forgotPasswordFetchedData.invalidEmail
+    }
+
+    else if (forgotPasswordFetchedData.resetSuccess){
+        forgotPasswordMessage.style.color = "green"
+        forgotPasswordMessage.innerHTML = forgotPasswordFetchedData.resetSuccess
+        localStorage.setItem("token", JSON.stringify(forgotPasswordFetchedData.forgotPasswordResetToken))
+
+        setTimeout(()=>{forgotPasswordForm.reset()}, 2000)
+    }
+
+    else if (forgotPasswordFetchedData.unverifiedEmail){
+        forgotPasswordMessage.style.color = "red"
+        forgotPasswordMessage.innerHTML = forgotPasswordFetchedData.unverifiedEmail
+    }
+
+    else if(forgotPasswordFetchedData.validationError){
+        forgotPasswordMessage.style.color = "red"
+        forgotPasswordMessage.innerHTML = forgotPasswordFetchedData.validationError
+    }
+
+    else {
+        forgotPasswordMessage.style.color = "red"
+        forgotPasswordMessage.innerHTML = forgotPasswordFetchedData.errorMessage
+    }
+})
+
+}
---
+++ JS/resetPassword.js
@@ -0,0 +1,65 @@
+
+const resetPasswordForm = document.getElementById("resetPasswordForm");
+
+const resetPasswordSubmitData = document.getElementById("resetPasswordSubmitData");
+
+const resetPasswordMessage = document.getElementById("resetPasswordMessage");
+resetPasswordMessage.style.display = "none";
+
+resetPasswordSubmitData.addEventListener("click", (event) =>{
+    event.preventDefault();
+    resetPasswordMessage.style.display = "block";   
+    resetPasswordMessage.innerHTML = `<img src="../images/Spinner.gif" alt="Loading..." width="50px" height="50px">`
+
+    forgotPassword();
+});
+
+
+function forgotPassword(){
+    
+    const resetPassword = document.getElementById("resetPassword");
+    const resetRepeatPassword = document.getElementById("resetRepeatPassword");
+
+    const data = {
+        password: resetPassword.value,
+        repeatPassword: resetRepeatPassword.value
+    }
+
+
+    const sendData = {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: new Headers({'Content-Type': 'application/json; charset=UTF-8', "auth_token": JSON.parse(localStorage.getItem("token"))})
+    }
+
+fetch("http://localhost:5000/login/newPassword", sendData)
+.then(response => response.json())
+.then((newPasswordFetchedData)=>{
+    console.log(newPasswordFetchedData)
+
+    if (newPasswordFetchedData.message){
+        resetPasswordMessage.style.color = "red"
+        resetPasswordMessage.innerHTML = newPasswordFetchedData.message
+    }
+
+    else if (newPasswordFetchedData.newPasswordSuccess){
+        resetPasswordMessage.style.color = "green"
+        resetPasswordMessage.innerHTML = newPasswordFetchedData.newPasswordSuccess
+
+        resetPasswordForm.reset()
+
+        setTimeout(()=>{location="login.html"}, 2000)
+    }
+
+    else if(newPasswordFetchedData.validationError){
+        resetPasswordMessage.style.color = "red"
+        resetPasswordMessage.innerHTML = newPasswordFetchedData.validationError
+    }
+
+    else {
+        resetPasswordMessage.style.color = "red"
+        resetPasswordMessage.innerHTML = newPasswordFetchedData.errorMessage
+    }
+})
+
+}
