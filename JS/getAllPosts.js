4bc29090b983b837cc020e104d06c5cefc536770
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2022-12-16 10:46:50 -0800

    changed the hosting server

--- JS/getAllPosts.js
+++ JS/getAllPosts.js
@@ -120,7 +120,7 @@ async function getAllPosts(){
         headers: {"auth_token": JSON.parse(sessionStorage.getItem("token"))}
     }
 
-    let response = await fetch("https://ernestruzindana-be.up.railway.app/getAllPosts", getData)
+    let response = await fetch("https://ernestruzindana-be.cyclic.app/getAllPosts", getData)
     const fetchedData = await response.json()
     hideBlogLoader()
     posts = fetchedData.allAvailablePosts;
