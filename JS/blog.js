df23ced75e89335b076cce0091ee3e9274a9c412
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2023-01-23 23:34:35 -0800

    my portfolio last commit

--- JS/blog.js
+++ JS/blog.js
@@ -1,22 +1,3 @@
-//Phone Navigation Bar
-
-const toggleButton = document.getElementById ("toggleButton");
-const closeButton = document.getElementById("closeButton");
-const nav = document.getElementById('menu')
-nav.classList.remove('menu-btn');
-
-
-toggleButton.addEventListener ("click", function(){
-    nav.classList.add('menu-btn');
-})
-
-
-
-nav.addEventListener("click", function(){
-    nav.classList.remove('menu-btn');
-})
-
-
 //Homeboxes Horizontal Scrollbar
 
 const sliders = document.querySelector('.homeboxes');
